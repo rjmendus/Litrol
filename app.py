c7c092f8712338196320f9d5a3b792b68a598d3c
Rajat Mendus <jaicmendus@gmail.com>
2018-07-09 20:57:53 +0530

    Change response for price for last 7 days

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import datetime as dt
 from keras.models import model_from_json
 from sklearn.preprocessing import MinMaxScaler
 
+sc=MinMaxScaler()
+
 app = Flask(__name__)
 
 
@@ -43,11 +45,12 @@ def get_price():
 def get_price_of_last_7_days():
 	if request.method == 'GET':
 		df = pd.read_csv('static/dataset/DelhiPrice.csv')
-		items = {}
+		items = [{},{},{},{},{},{},{}]
 		today = dt.datetime.now()
 		for i in reversed(range(7)):
-			items[(today - dt.timedelta(days=i)).strftime('%d-%m-%Y')] = str(df['Weighted_Price'][i])
-		return jsonify(items)
+			items[6-i] = {"date": (today - dt.timedelta(days=i)).strftime('%d-%m-%Y'),
+			"price": str(df['Weighted_Price'][i])}
+		return jsonify(Items=items)
 
 # @app.route('/predictforweek', methods=['GET'])
 # def get_predictions_for_a_week():
@@ -67,8 +70,6 @@ def upload_model_json():
 	return "Error"
 
 def predict_petrol_price():
-
-	sc=MinMaxScaler()
 	df=pd.read_csv('static/dataset/DelhiPrice.csv')
 	df['date']=pd.to_datetime(df['Timestamp'], unit='s').dt.date
 	group=df.groupby('date')
@@ -102,6 +103,55 @@ def predict_petrol_price():
 	predicted_Petrol_price=predicted_Petrol_price.tolist()
 	print(predicted_Petrol_price[-1][0])
 	return predicted_Petrol_price[-1][0]
+
+# def predict_petrol_price_week():
+# 	week=[]
+# 	df=pd.read_csv('static/dataset/DelhiPrice.csv')
+# 	df['date']=pd.to_datetime(df['Timestamp'], unit='s').dt.date
+# 	group=df.groupby('date')
+# 	Real_Price=group['Weighted_Price'].mean()
+# 	prediction_days=30
+# 	df_train=Real_Price[:len(Real_Price) - prediction_days]
+# 	df_test=Real_Price[len(Real_Price) - prediction_days:]
+# 	training_set=df_train.values
+# 	training_set=np.reshape(training_set, (len(training_set), 1))
+# 	training_set=sc.fit_transform(training_set)
+# 	# load json and create model
+# 	json_file=open('static/models/model.json', 'r')
+# 	loaded_model_json=json_file.read()
+# 	json_file.close()
+# 	loaded_model=model_from_json(loaded_model_json)
+# 	# load weights into new model
+# 	loaded_model.load_weights("static/models/model.h5")
+# 	print("Loaded model from disk")
+# 	# Fitting the RNN to the Training set
+# 	regressor=loaded_model
+# 	# Making the predictions
+# 	test_set=df_test.values
+# 	print("test Values", test_set)
+# 	inputs=np.reshape(test_set, (len(test_set), 1))
+# 	inputs=sc.transform(inputs)
+# 	print(sc.inverse_transform(inputs), type(inputs))
+# 	inputs=np.reshape(inputs, (len(inputs), 1, 1))
+# 	predicted_Petrol_price=regressor.predict(inputs)
+# 	predicted_Petrol_price=sc.inverse_transform(predicted_Petrol_price)
+# 	print("Predicted Price\n", predicted_Petrol_price)
+# 	price=predicted_Petrol_price[prediction_days-1]
+# 	for i in range(6):
+# 		print("DEBUG:",price[0],price,"\n")
+# 		week.append(price[0])
+# 		price=predict_point_by_point(regressor,price)
+# 	return week
+
+# def predict_point_by_point(model, data):
+#     # Predict each timestep given the last sequence of true data, in effect only predicting 1 step ahead each time
+#     inputs = np.reshape(data, (len(data), 1))
+#     inputs = sc.transform(inputs)
+#     inputs = np.reshape(inputs, (len(inputs), 1, 1))
+#     predicted = model.predict(inputs)
+#     predicted = sc.inverse_transform(predicted)
+#     print(predicted)
+#     return predicted
 
 if __name__ == "__main__":
 	app.run(debug=True)
