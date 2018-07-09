a33ca5edf248fa3afa702624a9bec3c1319d2ce6
Rajat Mendus <jaicmendus@gmail.com>
2018-07-09 11:45:22 +0530

    Add the predict get request

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return "Howdy! It's up!"
+
+@app.remote('/predict', methods=['GET','POST'])
+def predict():
+	if request.method == 'GET':
+		return "Get method is up."
+	
+	return "Success"
 
 if __name__ == "__main__":
 	app.run(debug=True)
